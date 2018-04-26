516da89787756d52e1ebcbae2e6e8e7c5c2523bb
jivan <jivanng2@gmail.com>
2018-04-26 13:14:58 +0500

    new

--- web.js
+++ web.js
@@ -1,7 +1,25 @@
 var express = require("express");
 var app = express();
+var path = require('path');
 // app.use(express.logger());
-app.use("/", express.static(__dirname));
+ app.use("/", express.static(__dirname));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/seminar', function(req, res) {
+  res.sendFile(path.join(__dirname + '/seminar.html'));
+});
+
+app.get('/dissertation', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dissertation.html'));
+});
+
+app.get('/project', function(req, res) {
+  res.sendFile(path.join(__dirname + '/project.html'));
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
